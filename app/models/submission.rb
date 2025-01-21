7dfac7b024db263b0aba7fd429419d990a0b5c9b
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-01-21 21:08:59 +0530

    Add Schema Conflicts

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,8 @@
 class Submission < ApplicationRecord
+<<<<<<< HEAD
   validates :sign_id, :submitted_by, :approved_by, presence: true
   has_many :user
   has_many :sign
+=======
+>>>>>>> f691cac (Add Schema)
 end
