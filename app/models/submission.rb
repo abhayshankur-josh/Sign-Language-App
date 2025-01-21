9c7f1aa4c5f18e248d22af611d02f7fcabfd5c9a
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-01-22 00:39:20 +0530

    Add validation and update relationship.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -22,10 +22,7 @@
 #  submitted_by  (submitted_by => users.id)
 #
 class Submission < ApplicationRecord
-<<<<<<< HEAD
   validates :sign_id, :submitted_by, :approved_by, presence: true
   has_many :user
   has_many :sign
-=======
->>>>>>> f691cac (Add Schema)
 end
