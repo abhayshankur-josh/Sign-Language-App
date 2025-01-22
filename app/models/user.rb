96eae16a58659287cc9555b73af2aaff1cd38d8b
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-01-23 01:56:05 +0530

    Add Contollers & Routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  full_name  :string
+#  password   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  role_id    :integer
+#
+# Indexes
+#
+#  index_users_on_role_id  (role_id)
+#
+# Foreign Keys
+#
+#  role_id  (role_id => roles.id)
+#
 class User < ApplicationRecord
   validates :full_name, :email, :password, :role_id, presence: true
   validates :email, uniqueness: true
