a88a19c1c0813e9fdddd72ca86ca2c146bec82f2
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-01-24 02:26:02 +0530

    Add Home index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Root path
+  root to: "home#index"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -15,5 +18,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  # Define resorces
   resources :users
 end
