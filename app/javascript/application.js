7020b7ccd284d391cc91c16d225af1765f442fd2
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-01-24 01:00:49 +0530

    Revert "Add esbuild config"
    This reverts commit b6db545321ccb8f21eed2fc2338c29eb8ce7d30b.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./controllers"
-
-// Adding Bootstraps dependencies
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "controllers"
