a2ecfd4b8258b2a1ab321254cdf4a4c1966cc860
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-01-23 16:09:23 +0530

    Add User Query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,34 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = user_query_class.get_all_users
   end
 
   def create
-    User.create!({
+    user_params = {
       full_name: params[:full_name],
       email: params[:email],
       password: params[:password],
       role_id: params[:role_id]
-    })
-    redirect_to(@users)
+    }
+    user_query_class.add_user(user_params)
   end
 
   def show
     # Extract the composite ID value from URL parameters.
     id = params.extract_value(:id)
-    @user = User.find(id).first
+    @user = user_query_class.get_user(id)
   end
 
   def edit
     # Extract the composite ID value from URL parameters.
     id = params.extract_value(:id)
-    @user = User.find(id).first
+    @user = user_query_class.get_user(id)
   end
 
   def new
+  end
+
+  def user_query_class
+    @user_query_class ||= UserQuery.new
   end
 end
