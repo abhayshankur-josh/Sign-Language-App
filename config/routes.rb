0e04f76670ebf50e836fc2d1c83258bd5d47fefc
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-01-28 03:04:42 +0530

    Create Devise and Scaffold for Admin. Designing queries for Roles, Admins & Users. Modify Database Schema with addition of Admins table.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 #
 
 Rails.application.routes.draw do
+  devise_for :admins
+  resources :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Root path
