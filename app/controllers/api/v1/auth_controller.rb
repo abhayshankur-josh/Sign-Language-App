c1f5b96d88d93257f5977b28c528327b7bf520cb
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-02-03 22:22:21 +0530

    Add session management for API.

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,14 +3,18 @@ require_relative "../json_web_token"
 class Api::V1::AuthController < Api::V1::ApplicationController
   before_action :authorize_request, except: [ :login, :signup ]
   before_action :login_params
-  skip_before_action :verify_authenticity_token
+  before_action :refresh_jti, only: :signout
+  # skip_before_action :verify_authenticity_token
 
   # POST /auth/login
   def login
     @user = User.find_by_email(params[:email])
     if @user&.valid_password?(params[:password])
       sign_in(:user, @user)
-      token = JsonWebToken.encode(user_id: @user.id)
+      jti_id = SecureRandom.uuid
+      @user.jti = jti_id
+      @user.save!
+      token = JsonWebToken.encode({ user_id: @user.id, jti_id: jti_id })
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                      username: @user.full_name }, status: :ok
@@ -26,7 +30,9 @@ class Api::V1::AuthController < Api::V1::ApplicationController
   # DELETE /auth/signout
   def signout
     sign_out @current_user
-    render json: { user: @current_user }, status: :ok
+    render json: { message: "Sign out successful." }, status: :ok
+  rescue Exception => e
+    render json: { errors: "Ooppszz.. Something went wrong!" }, status: :bad_request
   end
 
 
@@ -35,5 +41,15 @@ class Api::V1::AuthController < Api::V1::ApplicationController
 
   def login_params
     params.permit(:email, :password)
+  rescue Exception => e
+    render json: { errors: "Ooppszz.. Something went wrong!" }, status: :bad_request
+  end
+
+  def refresh_jti
+    @user = User.find(@current_user[:id])
+    @user.jti = SecureRandom.uuid
+    @user.save!
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.message }, status: :unauthorized
   end
 end
--- db/migrate/20250121151435_create_users.rb
+++ db/migrate/20250121151435_create_users.rb
@@ -3,10 +3,13 @@ class CreateUsers < ActiveRecord::Migration[7.2]
     create_table :users do |t|
       t.string :full_name
       t.integer :role_id, default: 01
+      t.string :jti
+      t.boolean :active, default: true
       t.timestamps
     end
 
     add_foreign_key :users, :roles, column: :role_id
     add_index :users, :role_id
+    add_index :users, :jti
   end
 end
